f803dbf31007d65018c6eff6997fd63f1782f82d
Ffranco08 <141684617+Ffranco08@users.noreply.github.com>
2023-09-05 22:00:17 +0000

    started coding

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from matplotlib import image
 from flask import Flask
 app = Flask(__name__)
+
+open(giesel.jpeg)
+    
 
 @app.route("/")
 def hello():
    return "Hello World!"
+
+#actually hosts the webserver
+if __name__ == "__main__":
+   app.run(host='0.0.0.0')
