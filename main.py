33260cce0d1f839381fa56024cffe3e6177a88d2
RicardoDiaz4 <141684760+RicardoDiaz4@users.noreply.github.com>
2023-09-07 23:26:08 +0000

    website ver 1

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from matplotlib import image
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-open(giesel.jpeg)
-    
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/')
+def home2():
+    return render_template('index.css')
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/')
+def song_input():
+    song = str(input())
+    return song
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route("/")
 def hello():
-   return "Hello World!"
-
+    return "Hello World!"
 #actually hosts the webserver
 if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
